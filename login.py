2bb6a7322da77d8e92929dda43063de4bcdb3d6c
Maksym Kozlovskyi <zxcshaft@gmail.com>
2026-03-16 20:57:45 +0100

    Python registration service with json integration v1.0

---
+++ login.py
@@ -0,0 +1,91 @@
+import os 
+import json
+
+class Pass:
+        
+    def registration(self):
+
+        with open("users.json", "r") as f:
+            users = json.load(f)
+
+        us = input("Fill this gap with your username:\nIt need to be beetween 10-15 char: ")
+        if len(us) < 10 > 20:
+            print("Username has wrong lenth, it need to be from 10 to 20 char!")
+        elif self.us_check(us):
+            print("Username already exists")
+        else:
+            pass
+
+        passw = input("Fill this gap with your password:\nIt need to be between 10-15 char: ")
+        if len(passw) <10 > 15:
+            print("Password has wrong lenth, it need to be from 10 to 20 char!")
+
+        new_user = {
+            "id": 0+1,
+            "username": us,
+            "passwoed": passw
+        }
+        users.append(new_user)
+
+        with open("todos.json", "w", encoding = "utf8") as f:
+            json.dump(users, f, indent = 4)
+
+        print("You was successfully registrated!")
+
+
+    def us_check(self, x):
+        users = open("users.json", "r")
+        data = json.load(users)  
+        for us in data['users']:
+            if x == us['username']:
+                return True
+        return False
+
+    def is_reg_check(self,x ):
+        users = open("users.json", "r")
+        data = json.load(users)
+        for usernm in data['users']:
+            if x != usernm['username']:
+                return True
+        return False
+    
+    def log_in(self):
+        usernm = input("Fill this gap with your username:")
+        if self.is_reg_check(usernm):
+            ad_reg = int(input("You are not registered yet;/\nInput 1 if you want to"))
+            if ad_reg == 1:
+                self.registration()
+            else:
+                pass
+        else:
+            with open("users.json", "r") as f:
+                users = json.load(f)
+            
+
+        
+        
+
+        pas = input("FIll this gap with your password")
+        
+def init_file():
+    if not os.path.exists("users.json"):
+        with open("users.json", "w") as f:
+            json.dump([], f)
+
+def main():
+    app = Pass()
+    while True:
+        with open("data.json") as f:
+            data = json.load(f)
+
+        log = input("Print 1 if you already have account.\nPrint  2 if you want to registrate an account:")
+        if log == 1:
+            app.log_in()
+        elif log == 2:
+            app.registration()
+        elif log != 1 and 2:
+            print("Error input")
+
+
+init_file()
+main()
